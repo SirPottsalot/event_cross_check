3a7e14d6b7e618e453586af56ac711ccb1076edf
SirPottsalot <potts.matt.93@gmail.com>
2019-12-08 23:11:37 -0700

    added function to find commonalities

--- event_compare.py
+++ event_compare.py
@@ -1,6 +1,10 @@
-
-
+#! /bin/env python3
+# import librarys
 import argparse
+
+# function
+def AutocraticCommonalityDectectorino(a,b,c):
+    return ['Date: {} Time: {} Time_Diff: {}'.format((x[0]),(y[1][0:-1]),str(round(abs(float(x[1])-float(y[1])),9)))  for x in a for y in b  if x[0] == y[0] and round(abs(float(x[1])-float(y[1])),9) <= c]
 
 #Get arguments from command line to get path to file
 parser = argparse.ArgumentParser(description = 'This script is designed to look at the event lists times from FD and SD to find coincidence events.')
@@ -11,3 +15,10 @@ args = parser.parse_args()
 # Store all varibles into lists
 sd_events = [x.split(' ')[1:3] for x in open(args.SD,'r').readlines()]
 fd_events = [x.split(' ')[1:3] for x in open(args.FD,'r').readlines()]
+
+# Call function
+AutocraticCommonalityDectectorino(sd_events,fd_events,1)
+
+
+# return [' '.join(x[0:]) + ' ' +' '.join(y[0:]) for x in a for y in b  if x[0] == y[0] and abs(float(x[1])-float(y[1])) <= c]
+#return ['Date: ' + ''.join(x[0]) + ' Time: ' +''.join(y[1]) for x in a for y in b  if x[0] == y[0] and abs(float(x[1])-float(y[1])) <= c]
